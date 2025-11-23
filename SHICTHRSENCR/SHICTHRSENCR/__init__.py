9c92208b65e6a9f43504008760c5765c3b3e1d23
JNTMTMTM <15251865468@163.com>
2025-11-23 20:38:15 +0800

    V0.0.0 添加.gitignore和LICENSE文件

---
+++ SHICTHRSENCR/SHICTHRSENCR/__init__.py
@@ -0,0 +1,14 @@
+# *-* coding: utf-8 *-*
+# src\__init__.py
+# SHICTHRS JSON LOADER
+# AUTHOR : SHICTHRS-JNTMTMTM
+# Copyright : © 2025-2026 SHICTHRS, Std. All rights reserved.
+# lICENSE : GPL-3.0
+
+import os
+from colorama import init
+init()
+
+print('\033[1mWelcome to use SHRENCR - ENCR System\033[0m\n|  \033[1;34mGithub : https://github.com/JNTMTMTM/SHICTHRS_ENCR\033[0m')
+print('|  \033[1mAlgorithms = rule ; Questioning = approval\033[0m')
+print('|  \033[1mCopyright : © 2025-2026 SHICTHRS, Std. All rights reserved.\033[0m\n')
